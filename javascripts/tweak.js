df4880c40aea4722d2c39cdeb7de193239aa0e32
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-11 09:13:37 -0700

    Update tweak.js

--- javascripts/tweak.js
+++ javascripts/tweak.js
@@ -3,4 +3,4 @@ for (x in $(".slide")) {
     cl = '.' + $(".slide")[x].classList[1].toString
     if ($(cl).height() < (window.innerHeight + 45)) {
       $(cl).css("height", (window.innerHeight + 45).toString().concat('px'))
-    }});
+    }};
