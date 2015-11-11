220351a5e4e6b8df22bd20e0cd8abdcc435f5580
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-11 08:36:54 -0700

    Update tweak.js

--- javascripts/tweak.js
+++ javascripts/tweak.js
@@ -1,5 +1,5 @@
 $(".slide").each(function(i, obj) {
     console.log(obj)
     if ($(obj.class).height() < (window.innerHeight + 45)) {
-      obj.css("height", (window.innerHeight + 45).toString().concat('px'))
+      $(obj.class).css("height", (window.innerHeight + 45).toString().concat('px'))
     }});
