2ab60f84e2c94a3b3a801f6a879cc717ddff68f0
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-14 13:15:48 -0700

    Update blog-main.js

--- blog/blog-main.js
+++ blog/blog-main.js
@@ -1,1 +1,4 @@
-null
+var blog = json.parse($.get('https://dylan5797.github.io/blog/blogdata.json').responseText)
+for (x in blog[0]) {
+  $('.playground').append('<p>' + blog[0][x]["postName"] + '</p>')
+};
