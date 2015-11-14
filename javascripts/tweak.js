5f875bcbc974ee240cf0c603e854f9e0a6a2a262
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-14 07:53:35 -0700

    Delete tweak.js

--- javascripts/tweak.js
+++
@@ -1,6 +0,0 @@
-var cl = -1
-for (x in $(".slide")) {
-    cl = '.' + $(".slide")[x].classList[1].toString
-    if ($(cl).height() < (window.innerHeight + 45)) {
-      $(cl).css("height", (window.innerHeight + 45).toString().concat('px'))
-    }};
