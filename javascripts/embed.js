4bbb2f315b483bc39b7d34b5379fcf8a3c11944e
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-14 08:13:39 -0700

    Update embed.js

--- javascripts/embed.js
+++ javascripts/embed.js
@@ -4,4 +4,6 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-$('.player-target').append('<iframe allowtransparency="true" width="485" height="402" src="https://scratch.mit.edu/projects/embed/" + getParameterByName("id") + "/?autostart=false" frameborder="0" allowfullscreen></iframe>')
+$(document).ready(function(){
+  $('.player-target').append('<iframe allowtransparency="true" width="485" height="402" src="https://scratch.mit.edu/projects/embed/' + getParameterByName("id") + '/?autostart=false" frameborder="0" allowfullscreen></iframe>');
+});
