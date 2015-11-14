11c148ae71902930cc471554e141cbaf8b228e13
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-14 08:08:54 -0700

    Create embed.js

---
+++ javascripts/embed.js
@@ -0,0 +1,7 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+$('.player-target').append('<iframe allowtransparency="true" width="485" height="402" src="https://scratch.mit.edu/projects/embed/" + getParameterByName("id") + "/?autostart=false" frameborder="0" allowfullscreen></iframe>')
