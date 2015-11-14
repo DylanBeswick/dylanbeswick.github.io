c9d59bb2d3ffb9bd627d27fbedaa5dd7a7a48572
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-14 15:27:54 -0700

    Update blog-main.js

--- blog/blog-main.js
+++ blog/blog-main.js
@@ -1,7 +1,11 @@
 function callback(data) {
- var blog = data["blog"]
+ var blog = data["blog"];
+ var ts;
+ month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 for (x in blog[0]) {
-  $('.playground').append('<a class="blog-link">' + blog[0][x]["postName"] + '</a>')
+  $('.playground').append('<a class="blog-link">' + blog[0][x]["postName"] + '</a>');
+  ts = blog[0][x]["timestamp"]
+  $('.playground').append('<p>' + month[ts.slice(4, 6) - 1] + '. ' + ts.slice(6,8))
 };
 
 }
