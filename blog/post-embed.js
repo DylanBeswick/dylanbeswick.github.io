b4608f9d24623c02d5762454bb3107a1f2d80a1f
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-18 06:46:36 -0700

    Update post-embed.js

--- blog/post-embed.js
+++ blog/post-embed.js
@@ -1,19 +1,24 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+ 
 function callback(data) {
- var blog = data["blog"];
+var blog = data
  var ts;
  month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-for (x in blog) {
- ts = blog[x]["timestamp"]
-  $('.playground').append('<li class="' + ts + ' blogitem"></li>'); 
-  $('.' + ts).append('<a class="blog-link">' + blog[x]["postName"] + '</a>');
-  $('.' + ts).append('<p>' + month[ts.slice(4, 6) - 1] + '. ' + ts.slice(6,8))
-};
+  $('.playground').append('<div class="blog-main style="width:100%"></div>'); 
+  $('.blog-main').append('<h1 class="blog-title">' + blog["title"] + '</h1>');
+  ts = blog['timestamp']
+  $('.blog-main').append('<p style="margin-bottom:25px; color:#555555">' + month[ts.slice(4, 6) - 1] + '. ' + ts.slice(6,8))
+  $('.blog-main').append(blog['content'])
 
 }
 $(document).ready(function(){
 $.ajax({
      type: 'GET',
-     url: 'https://dylan5797.github.io/blog/blogdata.json',
+     url: 'https://dylan5797.github.io/blog/posts/' + getParameterByName("p") + '.json',
      success: callback
 });
 });
