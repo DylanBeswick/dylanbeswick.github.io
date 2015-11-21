015c1672ec2c26417f20e9d08fe2260a7e4665b8
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-21 09:41:33 -0700

    Update post-embed.js

--- blog/post-embed.js
+++ blog/post-embed.js
@@ -8,9 +8,9 @@ function callback(data) {
 var blog = JSON.parse(data);
  var ts;
  month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]; 
-  $('.playground').append('<h1 class="blog-title">' + blog["title"] + '</h1>');
+  $('.title-wrap').append('<h1 class="blog-title">' + blog["title"] + '</h1>');
   ts = blog['timestamp'];
-  $('.playground').append('<p style="margin-bottom:25px; color:#555555">' + month[ts.slice(4, 6) - 1] + '. ' + ts.slice(6,8));
+  $('.title-wrap').append('<p style="margin-bottom:25px; color:#555555">' + month[ts.slice(4, 6) - 1] + '. ' + ts.slice(6,8));
   $('.playground').append(blog['content']);
   $('.blog-title').css('padding-bottom', "5px");
   $('.blog-title').css('font-size', "40px");
