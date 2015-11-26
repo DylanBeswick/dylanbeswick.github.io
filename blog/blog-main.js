83ad0dd49ad6acdf9cf4b0faba9dbdfdc598f38c
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-11-25 17:56:55 -0700

    Update blog-main.js

--- blog/blog-main.js
+++ blog/blog-main.js
@@ -3,8 +3,8 @@ function callback(data) {
  var blog = JSON.parse(data)["blog"];
  var ts;
  var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+   clearInterval(intr)
   $('.loader-icon').remove()
-  $('body').css('background-color', '#F5F5FF')
 for (x in blog) {
  ts = blog[x]["timestamp"]
   $('.playground').append('<li class="' + ts + ' blogitem"></li>'); 
@@ -14,8 +14,22 @@ for (x in blog) {
 
 }
 $(document).ready(function(){
-$('body').css('background-color', '#FFFFFF')
-$('.playground').append('<img src="https://cloud.githubusercontent.com/assets/13187260/11357268/0a7a0be2-9223-11e5-9832-c2e5bca37e38.gif" class="loader-icon">')
+$('.playground').append('<div class="loader-icon" style="height:110px; width:165px; background-color:#F5F5FF"><div class="bar bar1" style="width: 15px; float: left; margin-left: 5px; height: 15.669px; background-color: rgb(9, 221, 153);"></div><div class="bar bar2" style="width: 15px; float: left; margin-left: 5px; height: 65.4654px; background-color: rgb(9, 221, 153);"></div><div class="bar bar3" style="width: 15px; float: left; margin-left: 5px; height: 89.2195px; background-color: rgb(9, 221, 153);"></div><div class="bar bar4" style="width: 15px; float: left; margin-left: 5px; height: 87.5865px; background-color: rgb(9, 221, 153);"></div><div class="bar bar5" style="width: 15px; float: left; margin-left: 5px; height: 97.5797px; background-color: rgb(9, 221, 153);"></div><div class="bar bar6" style="width: 15px; float: left; margin-left: 5px; height: 42.5873px; background-color: rgb(9, 221, 153);"></div><div class="bar bar7" style="width: 15px; float: left; margin-left: 5px; height: 100.896px; background-color: rgb(9, 221, 153);"></div><div class="bar bar8" style="width: 15px; float: left; margin-left: 5px; height: 98.3674px; background-color: rgb(9, 221, 153);"></div></div>')
+var i = 0
+var p = []
+var x = 0
+while (x < document.getElementsByClassName("bar").length) {
+p.push(Math.random() * 10)
+x++
+}
+function frame() {
+i++
+for (var c in p) {
+    var m = document.getElementsByClassName("bar" + (parseInt(c, 10) + 1))[0].style
+    m.height = (Math.abs(Math.sin((i + c / 15) / 10 + p[c]) * 100) + 1).toString() + "px"
+}
+}
+var intr = setInterval(frame, 20)
 $.ajax({
      type: 'GET',
      url: 'https://raw.githubusercontent.com/Dylan5797/dylan5797.github.io/master/blog/blogdata.json',
