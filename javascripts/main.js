d2b72beefa7a87da39b078e5487815ebef426336
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-12-07 20:59:04 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ if (window.location.pathname != '/') {
   $( document ).ready(function() {
     $('body').prepend(`<div class="navbar">
         <dl class="sub-nav">
-        	  <span class="navlink logonav"><a href="#front">Dylan Beswick</a></span>
+        	  <span class="navlink logonav"><a href="https://dylanbeswick.github.io/#front">Dylan Beswick</a></span>
             <span class="navlink"><a href="https://dylanbeswick.github.io/#about">About</a></span>
             <span class="navlink"><a href="https://dylanbeswick.github.io/#coding">Coding</a></span>
             <span class="navlink"><a href="https://dylanbeswick.github.io/#codingprojects">Coding Projects</a></span>
