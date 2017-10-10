61dcd3aaf7e2c9b6859c9cf20cbf1cd2d73dee22
Dylan Beswick <djbeswick64@gmail.com>
2017-10-09 21:56:30 -0700

    welp..

--- javascripts/embed.js
+++
@@ -1,9 +0,0 @@
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-$(document).ready(function(){
-  $('.player-target').append('<iframe allowtransparency="true" width="485" height="402" src="https://scratch.mit.edu/projects/embed/' + getParameterByName("id") + '/?autostart=false" frameborder="0" allowfullscreen></iframe>');
-});
