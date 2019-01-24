6023c423f85739983ea6124329b26ae412233b18
PolyEdge <djbeswick64@gmail.com>
2019-01-24 16:58:51 -0700

    update site

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-
-// navbar
-if (window.location.pathname != '/') {
-  $(document).ready(function() {
-    $('body').prepend(`<div class="navbar-wrapper"><div class="navbar"><div class="nav-inner"><a href="/">Dylan Beswick</a><p class="nav-sep"></p><a href="/writing">Writing</a><p class="nav-sep"></p><a href="https://github.com/PolyEdge">GitHub</a><p class="nav-sep"></p><a href="https://soundcloud.com/dylan-beswick">SoundCloud</a></div></div><img class="nav-img" src="/static/img/navbar-tab.png" width="65px" height="65px"></div>`)
-})
-}
